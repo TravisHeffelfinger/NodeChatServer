c725c0fd4f445507f64f8b5d620224c798030ebd
TravisHeffelfinger <travisjohn1011@gmail.com>
2020-07-22 16:29:28 -0600

    Client Handling Changes
    added Client class in order to avoid errors with client handling

--- server.js
+++ server.js
@@ -4,23 +4,39 @@ const net = require("net");
 process.stdin.setEncoding("utf8");
 
 let connectedUsers = [];
-
+class Client {
+  constructor(name, id, socket) {
+    this.name = name;
+    this.id = id;
+    this.socket = socket;
+  }
+}
+function idGenerator() {
+  return Math.floor(Math.random() * 1000000);
+}
 const server = net.createServer((socket) => {
-  socket.write("Welcome to the server!");
-  connectedUsers.push(socket);
-  console.log(`there are ${connectedUsers.length} connected`);
+  let client = new Client(`Guest${connectedUsers.length + 1}`, idGenerator() ,socket);
+  connectedUsers.push(client);
+  socket.write(`Welcome to the server ${client.name}!`);
+  console.log(`connections: ${connectedUsers.length}`);
 
-  socket.on("end", socket.end);
+  socket.on("end", () => {
+    connectedUsers.filter((user, index) => {
+      user.id !== client.id ? user.socket.write(`${client.id} has disconnected`): connectedUsers.splice(index);
+    });
+    
+  });
 
   socket.on("error", (err) => {
     console.log(err);
   });
-  socket.on("data", (input) => {
-    console.log(input.toString().trim());
-    connectedUsers.filter((index) => {
-      if (index !== socket) socket.write(input);
+  socket.on("data", (data) => {
+    let message = data.toString().trim();
+    console.log(message);
+    connectedUsers.filter((user) => {
+      if (user.id !== client.id) user.socket.write(`${client.name}: ${message}`);
     });
   });
 });
 
-server.listen(3000);
+server.listen(3000, () => console.log('server listening on port: 3000'));
